fbb0b19bb2689dcf5e9f1d63d723e556041ea6fc
Senpaiyaa <ignacioerwin00@gmail.com>
2020-08-06 17:05:01 +0800

    vue directives to conditionally render elements

---
+++ lessons/conditional-rendering/main.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+
+	el: '#app',
+	data: {
+		product: 'Socks',
+		image: './assets/vmSocks-green-onWhite.jpg',
+		// inventory: 0
+		// inStock: false
+
+		// the challenge part
+		onSale: false
+	}
+
+})
